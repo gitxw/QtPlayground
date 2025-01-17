e38e0b1656d8c363e12d967d6300a52af23da3bd
XuWei <xiaohuoma7620@163.com>
2025-01-17 11:35:30 +0800

    测试图像的路径移到应用程序目录。

--- QtPlayground/OpenGLWidgetTest/mainwindow.cpp
+++ QtPlayground/OpenGLWidgetTest/mainwindow.cpp
@@ -1,16 +1,21 @@
 ﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QApplication>
+#include <QDir>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    QImage img_1(R"(D:\Code\git\QtPlayground\_TestData\30M_images\right01.jpg)");
-    QImage img_6(R"(D:\Code\git\QtPlayground\_TestData\30M_images\right06.jpg)");
-    QImage img_7(R"(D:\Code\git\QtPlayground\_TestData\30M_images\right07.jpg)");
-    QImage img_8(R"(D:\Code\git\QtPlayground\_TestData\30M_images\right08.jpg)");
+    // 需要提前将_TestData/30M_images拷贝到程序运行路径下
+    QString img_dir = QDir::cleanPath(QApplication::applicationDirPath() + "/30M_images");
+    QImage img_1(QDir::cleanPath(img_dir + "/right01.jpg"));
+    QImage img_6(QDir::cleanPath(img_dir + "/right06.jpg"));
+    QImage img_7(QDir::cleanPath(img_dir + "/right07.jpg"));
+    QImage img_8(QDir::cleanPath(img_dir + "/right08.jpg"));
     m_img_list.emplaceBack(img_1);
     m_img_list.emplaceBack(img_6);
     m_img_list.emplaceBack(img_7);
